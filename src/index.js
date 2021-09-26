99450f9e2864a0cd96d61f4d59d3c6760491de36
SakibAshraf <sakibashraf5898@gmail.com>
2021-09-27 05:37:32 +0600

    made PWA

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 
-import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -11,7 +10,59 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-serviceWorker.register();
+const divInstall = document.getElementById('installContainer');
+const butInstall = document.getElementById('butInstall');
+
+/* Put code here */
+window.addEventListener('beforeinstallprompt', (event) => {
+	console.log('👍', 'beforeinstallprompt', event);
+	// Stash the event so it can be triggered later.
+	window.deferredPrompt = event;
+	// Remove the 'hidden' class from the install button container
+	divInstall.classList.toggle('hidden', false);
+});
+butInstall.addEventListener('click', async () => {
+	console.log('👍', 'butInstall-clicked');
+	const promptEvent = window.deferredPrompt;
+	if (!promptEvent) {
+		// The deferred prompt isn't available.
+		return;
+	}
+	// Show the install prompt.
+	promptEvent.prompt();
+	// Log the result
+	const result = await promptEvent.userChoice;
+	console.log('👍', 'userChoice', result);
+	// Reset the deferred prompt variable, since
+	// prompt() can only be called once.
+	window.deferredPrompt = null;
+	// Hide the install button.
+	divInstall.classList.toggle('hidden', true);
+});
+window.addEventListener('appinstalled', (event) => {
+	console.log('👍', 'appinstalled', event);
+	// Clear the deferredPrompt so it can be garbage collected
+	window.deferredPrompt = null;
+});
+
+
+/* Only register a service worker if it's supported */
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/serviceWorker.js');
+}
+
+/**
+ * Warn the page must be served over HTTPS
+ * The `beforeinstallprompt` event won't fire if the page is served over HTTP.
+ * Installability requires a service worker with a fetch event handler, and
+ * if the page isn't served over HTTPS, the service worker won't load.
+ */
+if (window.location.protocol === 'http:') {
+  const requireHTTPS = document.getElementById('requireHTTPS');
+  const link = requireHTTPS.querySelector('a');
+  link.href = window.location.href.replace('http://', 'https://');
+  requireHTTPS.classList.remove('hidden');
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
